3feb19b1a94cc7a4143715f358190367bff50066
li_ye_hua@126.com <li_ye_hua@126.com>
2015-09-01 22:58:29 +0800

    modify the book page

--- classics/static/js/views/books/book.js
+++ classics/static/js/views/books/book.js
@@ -14,4 +14,32 @@ $(function () {
         type: 'fixed'
     });
 
+    $('#bb-nav-prev').on('click', function() {
+        var display_chapter = $('.chapter.content.display');
+        var prev_display_chapter = $('.chapter.content.display').prev('div');
+        display_chapter.fadeOut("slow");
+        display_chapter.removeClass('display');
+        prev_display_chapter.fadeIn("slow");
+        prev_display_chapter.addClass('display');
+    });
+
+    $('#bb-nav-next').on('click', function() {
+        var display_chapter = $('.chapter.content.display');
+        var next_display_chapter = $('.chapter.content.display').next('div');
+        display_chapter.fadeOut("slow");
+        display_chapter.removeClass('display');
+        next_display_chapter.fadeIn("slow");
+        next_display_chapter.addClass('display');
+    });
+
+    $('.ui.sidebar.menu .item').on('click', function() {
+        var $this = $(this);
+        var next_display_chapter_id = $this.attr('page-data');
+        var display_chapter = $('.chapter.content.display').fadeOut('slow');
+        display_chapter.removeClass('display');
+        var next_disaply_chapter = $('#' + next_display_chapter_id);
+        next_disaply_chapter.fadeIn('slow');
+        next_disaply_chapter.addClass('display');
+    });
+
 });
